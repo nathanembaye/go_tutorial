5fcec9d58c870d88161486821c1fdf0bb0a613a2
Nathan Embaye <nathanembaye26@gmail.com>
2021-02-19 17:29:00 -0500

    Variable tutorial

---
+++ var/var.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	
+	name:= "Nathan"
+    var age = 23
+
+	fmt.Println(name, age)
+	fmt.Printf("%T\n", name)
+}
+
+
+
+//string, bool, int
+//float32, float64
+//int8 (or byte), int16, int32 (or rune), int64
+//uint8, uint16, uint32, uint64
