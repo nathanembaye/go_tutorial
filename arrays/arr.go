46cd8dc6f1194e4ee73f83006e6a8ed0b492b35b
Nathan Embaye <nathanembaye26@gmail.com>
2021-02-19 23:26:14 -0500

    arrays in go

---
+++ arrays/arr.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    fruitArr := []string{"Apple", "Orange", "Grape"}
+
+	fmt.Println(fruitArr)
+	fmt.Println(len(fruitArr))
+	fmt.Println(fruitArr[1:2])
+
+
+}
